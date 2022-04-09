693dbfb27b985e5fb0be16ccd12f9c8f31d55ff2
enajera1 <eriknajera013@gmail.com>
2022-04-09 09:10:05 -0500

    almost done, need to make gen pw button work

--- Develop/asstets/js/script.js
+++ Develop/asstets/js/script.js
@@ -32,36 +32,65 @@ function passCase1 () {
   console.log(passCaseconfirm1);
 };
 
-// May not need this
-// function asking user if they want lower case characters or not
-//function passCase2() { 
-  //var passCaseconfirm2 = window.confirm("Do you want lower case characters?")
-      //if (passCaseconfirm2 === true) {
-        //window.alert("You have chosen to use lower case characters.")
-      //}else {
-        //window.alert("You have chosen NOT to use lower case characters.")
-      //}
+//function asking user if they want lower case characters or not
+function passCase2() { 
+  var passCaseconfirm2 = window.confirm("Do you want lower case characters?")
+      if (passCaseconfirm2 === true) {
+        window.alert("You have chosen to use lower case characters.")
+      }else {
+        window.alert("You have chosen NOT to use lower case characters.")
+      }
       //USE UPPERCASE ARRAY
     
-    //console.log(passCaseconfirm2);
-// May not need this
+    console.log(passCaseconfirm2);
+  }
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+// Function asking user for special characters or not
+function passChar()  {
+  var passCharconfirm = window.confirm("Do you want to use special characters or not?");
+  if (passCharconfirm) {
+    window.alert("You have chosen to use special characters");
+    // USE SPECIAL CHARACTERS ARRAY
+  } else{
+    window.alert("You have chosen NOT to use special characters");
+    // DON'T USE SPECIAL CHARACTERS ARRAY
+  }
+  console.log(passCharconfirm)
 }
+//Function asking user if they want to use numbers or not
+function passNum(){
+  var passNumconfirm = window.confirm("Do you want to use numbers or not?");
+  if (passNumconfirm) {
+    window.alert("You have chosen to use numbers");
+    //USE SPECIAL CHARACTERS ARRAY
+  } else {
+    window.alert("You have chosen NOT to use numbers");
+    //DONT USE SPECIAL CHARACTERS ARRAY
+  }
+  console.log(passNumconfirm);
+}  
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+const passwordEl = document.getElementById('password');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
 
+
+const randomFun = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+generateEl.addEventListener('click', () => {
+  const length = userinputLength.value;
+
+  console.log(length);
+});
 
 //random generators
 function getRandomLower() {
@@ -81,4 +110,33 @@ function getRandomSymbol () {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-userinputLength();
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+
+//}
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
+
+
+
+//writePassword();
+function run () {
+  userinputLength();
+  passCase1();
+  passCase2();
+  passChar();
+  passNum();
+}
+
+run ();
